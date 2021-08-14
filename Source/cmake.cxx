a23f9a3ed5f4b07da6d4475fb0a3995eaa7a8fdb
Johan Holmberg <holmberg556@gmail.com>
2021-08-14 21:42:15 +0200

    cleanup cmIncludeTreeActive use

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -2443,7 +2443,6 @@ int cmake::Run(const std::vector<std::string>& args, bool noconfigure)
     cmSystemTools::PutEnv("MAKEFLAGS=");
   }
 
-  //cmIncludeTreeActive includeTreeActive;
   {
     cmIncludeTreeLevel treeLevel("*PreLoadCMakeFiles", cmIncludeTreeLevel::FunctionType);
   }
