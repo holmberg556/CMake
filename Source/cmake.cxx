e209d1a1b957a53ad30791867879d69b5f260994
Johan Holmberg <holmberg556@gmail.com>
2021-08-14 21:42:08 +0200

    debugging ...

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -2188,7 +2188,6 @@ int cmake::Configure()
   auto profilingRAII = this->CreateProfilingEntry("project", "configure");
 #endif
 
-  cmIncludeTreeActive includeTreeActive;
   DiagLevel diagLevel;
 
   if (this->DiagLevels.count("deprecated") == 1) {
@@ -2645,6 +2644,10 @@ int cmake::Run(const std::vector<std::string>& args, bool noconfigure)
   this->PrintPresetEnvironment();
 #endif
 
+  {
+    cmIncludeTreeLevel treeLevel("*GetWorkingMode", cmIncludeTreeLevel::FunctionType);
+  }
+
   // In script mode we terminate after running the script.
   if (this->GetWorkingMode() != NORMAL_MODE) {
     if (cmSystemTools::GetErrorOccurredFlag()) {
@@ -2662,16 +2665,29 @@ int cmake::Run(const std::vector<std::string>& args, bool noconfigure)
     cmSystemTools::PutEnv("MAKEFLAGS=");
   }
 
+  //cmIncludeTreeActive includeTreeActive;
+  {
+    cmIncludeTreeLevel treeLevel("*PreLoadCMakeFiles", cmIncludeTreeLevel::FunctionType);
+  }
+
   this->PreLoadCMakeFiles();
 
   if (noconfigure) {
     return 0;
   }
 
+  {
+    cmIncludeTreeLevel treeLevel("*CheckBuildSystem", cmIncludeTreeLevel::FunctionType);
+  }
+
   // now run the global generate
   // Check the state of the build system to see if we need to regenerate.
   if (!this->CheckBuildSystem()) {
     return 0;
+  }
+
+  {
+    cmIncludeTreeLevel treeLevel("*Configure", cmIncludeTreeLevel::FunctionType);
   }
 
   int ret = this->Configure();
