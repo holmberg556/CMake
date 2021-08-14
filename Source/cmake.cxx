0076620be33159ef2cb460f0a69e0d3e625b5124
Johan Holmberg <holmberg556@gmail.com>
2021-08-14 21:42:00 +0200

    make cmIncludeTree only active during config

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -47,6 +47,7 @@
 #include "cmGeneratorTarget.h"
 #include "cmGlobalGenerator.h"
 #include "cmGlobalGeneratorFactory.h"
+#include "cmIncludeTree.h"
 #include "cmLinkLineComputer.h"
 #include "cmLocalGenerator.h"
 #include "cmMakefile.h"
@@ -2060,6 +2061,7 @@ int cmake::HandleDeleteCacheVariables(const std::string& var)
 
 int cmake::Configure()
 {
+  cmIncludeTreeActive includeTreeActive;
   DiagLevel diagLevel;
 
   if (this->DiagLevels.count("deprecated") == 1) {
